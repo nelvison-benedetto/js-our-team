bdb3c1c33acdb1bb72167102493122cfbb49c375
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-14 17:31:46 +0200

    uploading images

--- main.js
+++ main.js
@@ -3,36 +3,65 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./images/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./images/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./images/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./images/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./images/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./images/female3.png"
   }
 ];
+
+
+function generateTeam(member){
+  const {name,role,email,img} = member;
+
+  return `
+  <div class="col">
+    <div class="card text-center">
+        <img src="${img}" alt="" class="card-img-top">
+        <div class="card-body">
+            <h3>${name}</h3>
+            <span>${role}</span>
+            <small class="text-muted">${email}</small>
+        </div>
+    </div>
+  </div>`;
+}
+
+const team = document.getElementById("team");
+
+let totMarkup = "";
+teamMembers.forEach( (item,index) => {
+  const member = teamMembers[index];
+  const murkup = generateTeam(member);
+  totMarkup += murkup;
+});
+team.innerHTML = totMarkup ;
+console.log(totMarkup);
+
