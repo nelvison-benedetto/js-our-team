0420729241807d702b75a40543e0957242d65d97
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-14 17:34:41 +0200

    done

--- main.js
+++ main.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "./images/male1.png"
+    img: "./assets/images/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "./images/female1.png"
+    img: "./assets/images/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "./images/male2.png"
+    img: "./assets/images/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "./images/female2.png"
+    img: "./assets/images/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "./images/male3.png"
+    img: "./assets/images/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "./images/female3.png"
+    img: "./assets/images/female3.png"
   }
 ];
 
